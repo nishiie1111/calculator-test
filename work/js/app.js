a6c3ef8b33ffb874f9c3b16c1431eba59a076d63
nishiie <XC4feyt@gmail.com>
2026-02-03 21:32:30 +0900

    電卓アプリ js完成

---
+++ work/js/app.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const display = document.getElementById('display');
+  const numbers = document.querySelectorAll('.number');
+  const operators = document.querySelectorAll('.operator');
+  const equals = document.querySelector('.equals');
+  const clear = document.querySelector('.clear');
+
+  numbers.forEach((button) => {
+    button.addEventListener('click', () => {
+      display.value += button.dataset.number;
+    });
+  });
+
+  operators.forEach((button) => {
+    button.addEventListener('click', () => {
+      display.value += ` ${button.dataset.operator} `;
+    });
+  });
+
+  equals.addEventListener('click', () => {
+    display.value = eval(display.value);
+  });
+
+  clear.addEventListener('click', () => {
+    display.value = '';
+  });
+});
